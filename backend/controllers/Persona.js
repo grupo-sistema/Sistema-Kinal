d9d4db92a5b2e4d18c426046a51093371395ce49
JavierCF07 <33670804+JavierCF07@users.noreply.github.com>
2019-05-02 07:57:02 -0600

    Validaciones modulo persona

--- backend/controllers/Persona.js
+++ backend/controllers/Persona.js
@@ -1,137 +1,165 @@
-'use strict'
-var bcrypt = require('bcrypt-nodejs');
-var jwt = require('../services/jwt');
-var path = require('path');
-var fs = require('fs');
-var Persona = require('../models/Persona');
+"use strict";
+var bcrypt = require("bcrypt-nodejs");
+var jwt = require("../services/jwt");
+var path = require("path");
+var fs = require("fs");
+var Persona = require("../models/Persona");
 
+function InsertStudent(req, res) {
+  var student = new Persona();
+  var params = req.body;
+  var direccion = [];
+  var telefono = [];
+  if (
+    params.FirstName &&
+    params.SecondName &&
+    params.Surname &&
+    params.SecondSurname &&
+    params.Date &&
+    params.Religion &&
+    params.Email &&
+    params.Gender &&
+    params.Departament &&
+    params.Municipality &&
+    params.Zone &&
+    params.Colony &&
+    params.Avenue &&
+    params.Street &&
+    params.Block &&
+    params.HouseNumber &&
+    params.Mobile &&
+    params.Phone &&
+    params.Other
+  ) {
+    student = {
+      FirstName: params.FirstName,
+      SecondName: params.SecondName,
+      Surname: params.Surname,
+      SecondSurname: params.SecondName,
+      Date: params.Date,
+      Religion: params.Religion,
+      Email: params.Email,
+      Gender: params.Gender,
+      Address: (direccion = {
+        Departament: params.Departament,
+        Municipality: params.Municipality,
+        Zone: params.Zone,
+        Colony: params.Colony,
+        Avenue: params.Avenue,
+        Street: params.Street,
+        Block: params.Block,
+        HouseNumber: params.HouseNumber
+      }),
+      telephone: (telefono = {
+        Mobile: params.Mobile,
+        Phone: params.Phone,
+        Other: params.Other
+      })
+    };
+    Persona.findOne({ email: student.Email.toLowerCase() },(err, issetAdmin) => {
+          if (err) {
+            res.status(500).send({ message: "No se han guardado los datos correctamente" });
+          } else {
+            Persona.insertMany(student, function(err, studentSave) {
+            if (!issetAdmin) {
 
-function InsertStudent(req, res){
-    var student = new Persona();
-    var params = req.body;
-    var direccion = [];
-    var telefono = [];
-        if(params.FirstName && params.SecondName && params.Surname && params.SecondSurname
-            && params.Date && params.Religion && params.Email && params.Gender && params.Departament 
-            && params.Municipality && params.Zone && params.Colony && params.Avenue && params.Street 
-            && params.Block && params.HouseNumber && params.Mobile && params.Phone && params.Other){
-                student = {
-                FirstName: params.FirstName,
-                SecondName: params.SecondName,
-                Surname: params.Surname,
-                SecondSurname: params.SecondName,
-                Date: params.Date,
-                Religion: params.Religion,
-                Email: params.Email,
-                Gender: params.Gender,
-                Address : direccion ={
-                    Departament: params.Departament,
-                    Municipality: params.Municipality,
-                    Zone: params.Zone,
-                    Colony: params.Colony,
-                    Avenue: params.Avenue,
-                    Street: params.Street,
-                    Block: params.Block,
-                    HouseNumber: params.HouseNumber
-                },
-                telephone : telefono ={
-                    Mobile: params.Mobile,
-                    Phone: params.Phone,
-                    Other: params.Other
-                }
-                };
-                Persona.insertMany(student, function(err,studentSave){
-                    if(err){
-                        res.status(500).send({message: 'No se han guardado los datos correctamente'});
-                    }else{
-                        if(!studentSave){
-                            res.status(500).send({message: 'Error al guardar al alumno'});
-                        }else{
-                            res.status(200).send({student: studentSave});
-                        }
-                    }
-                });
-            }else{
-                res.status(404).send({message: 'Debe introducir los campos correctamente'});
+              if (!studentSave) {
+                res.status(500).send({ message: "Error al guardar al alumno" });
+              } else {
+                res.status(200).send({ student: studentSave });
+              }
+           
+            } else {
+              res.status(500).send({ message: "No puede guardar datos duplicados" });
             }
-        }
-
-
-
-
-function reportStudent(req,res){
-  
-    Persona.find/* Encuentra al usuario */({},(err,user)=>{
-        if(err){
-          console.log(err);
-            res.status(500).send({message: 'No se puede listar'});
-        }else{
-                res.status(200).send({user: user});
-            }
-    });
+        });
+          }
+      }
+    );
+  } else {
+    res.status(404).send({ message: "Debe introducir los campos correctamente" });
+  }
 }
-function studentUpdate(req,res){
-    var studentID = req.params.id;
-    var params = req.body;
-    var direccion = [];
-    var telefono = [];
-        Persona.updateMany({_id:studentID}, {$set: 
-            {
-                FirstName: params.FirstName,
-                SecondName: params.SecondName,
-                Surname: params.Surname,
-                SecondSurname: params.SecondName,
-                Date: params.Date,
-                Religion: params.Religion,
-                Email: params.Email,
-                Gender: params.Gender,
-                Address : direccion ={
 
-                    Departament: params.Departament,
-                    Municipality: params.Municipality,
-                    Zone: params.Zone,
-                    Colony: params.Colony,
-                    Avenue: params.Avenue,
-                    Street: params.Street,
-                    Block: params.Block,
-                    HouseNumber: params.HouseNumber
-                },
-                telephone : telefono ={
-                    Mobile: params.Mobile,
-                    Phone: params.Phone,
-                    Other: params.Other
-                }
-        }},{
-            new: true
-        },(err, studentUpdate)=>
-        {
-            if (err) {
-                res.status(500).send({message: 'Error al actualizar a la student.'});
-              }else {
-                if (!studentUpdate) {
-                  res.status(404).send({message: 'No se ha podido actualizar a la student.'});
-                }else {
-                  res.status(200).send({student: studentUpdate});
-                }
-              };
+function reportStudent(req, res) {
+  Persona.find(/* Encuentra al usuario */ {}, (err, user) => {
+    if (err) {
+      console.log(err);
+      res.status(500).send({ message: "No se puede listar" });
+    } else {
+      res.status(200).send({ user: user });
+    }
+  });
+}
+function studentUpdate(req, res) {
+  var studentID = req.params.id;
+  var params = req.body;
+  var direccion = [];
+  var telefono = [];
+  Persona.updateMany(
+    { _id: studentID },
+    {
+      $set: {
+        FirstName: params.FirstName,
+        SecondName: params.SecondName,
+        Surname: params.Surname,
+        SecondSurname: params.SecondName,
+        Date: params.Date,
+        Religion: params.Religion,
+        Email: params.Email,
+        Gender: params.Gender,
+        Address: (direccion = {
+          Departament: params.Departament,
+          Municipality: params.Municipality,
+          Zone: params.Zone,
+          Colony: params.Colony,
+          Avenue: params.Avenue,
+          Street: params.Street,
+          Block: params.Block,
+          HouseNumber: params.HouseNumber
+        }),
+        telephone: (telefono = {
+          Mobile: params.Mobile,
+          Phone: params.Phone,
+          Other: params.Other
         })
-
+      }
+    },
+    {
+      new: true
+    },
+    (err, studentUpdate) => {
+      if (err) {
+        res.status(500).send({ message: "Error al actualizar a la student." });
+      } else {
+        if (!studentUpdate) {
+          res
+            .status(404)
+            .send({ message: "No se ha podido actualizar a la student." });
+        } else {
+          res.status(200).send({ student: studentUpdate });
+        }
+      }
     }
-  
-  function deleteStudent(req, res){
-    var studentID = req.params.id;
-  
-            Persona.findByIdAndRemove(studentID,(err, Delete)=>{
-                if(err) return res.status(500).send({message: 'Error en la Peticion'});
-                if(!Delete) return res.status(404).send({message: 'No se ha podido borrar los Datos del Usuario'});
-                if (err) return next(err);
-                res.send('Se ha eliminado al Usuario Correctamente');
-            });
-
-    }
-module.exports = {
-    InsertStudent,
-    reportStudent,
-    deleteStudent,
-    studentUpdate
+  );
 }
+
+function deleteStudent(req, res) {
+  var studentID = req.params.id;
+
+  Persona.findByIdAndRemove(studentID, (err, Delete) => {
+    if (err) return res.status(500).send({ message: "Error en la Peticion" });
+    if (!Delete)
+      return res
+        .status(404)
+        .send({ message: "No se ha podido borrar los Datos del Usuario" });
+    if (err) return next(err);
+    res.send("Se ha eliminado al Usuario Correctamente");
+  });
+}
+module.exports = {
+  InsertStudent,
+  reportStudent,
+  deleteStudent,
+  studentUpdate
+};
--- sistema-kinal/src/app/components/inicio/inicio.component.ts
+++ sistema-kinal/src/app/components/inicio/inicio.component.ts
@@ -1,31 +1,36 @@
-import { Input, Component, OnInit } from '@angular/core';
-import { Person } from 'src/app/models/person';
-import { RestService } from '../../services/rest.service';
+import { Input, Component, OnInit } from "@angular/core";
+import { Person } from "src/app/models/person";
+import { RestService } from "../../services/rest.service";
+import { checkAndUpdateBinding } from "@angular/core/src/view/util";
+import { Message } from "@angular/compiler/src/i18n/i18n_ast";
 
 interface Alert {
   type: string;
   message: string;
 }
 
-const ALERTS: Alert[] = [{
-  type: 'success',
-  message: 'Datos enviados correctamente',
-}
+const ALERTS: Alert[] = [
+  {
+    type: "success",
+    message: "Datos enviados correctamente"
+  }
 ];
 
 @Component({
-  selector: 'app-inicio',
-  templateUrl: './inicio.component.html',
-  styleUrls: ['./inicio.component.css']
+  selector: "app-inicio",
+  templateUrl: "./inicio.component.html",
+  styleUrls: ["./inicio.component.css"]
 })
-
 export class InicioComponent implements OnInit {
   person: Person;
+  hola;
   alerts: Alert[];
-  
-  constructor(public rest: RestService) { 
+
+  constructor(public rest: RestService) {
     this.rest.setPerson(this.person);
-    this.person = new Person('','','','','','','','','','','','','','','','','','','','','');
+    this.person = new Person(
+      "","","","","","","","","","","","","","","","","","","","",""
+    );
   }
 
   close(alert: Alert) {
@@ -33,26 +38,53 @@ export class InicioComponent implements OnInit {
   }
 
   message() {
-    this.alerts = Array.from(ALERTS);
+
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  onSubmit(){
-    console.log(this.person);
-    this.rest.setPerson(this.person).subscribe(
-      response => {
-        if(response){
-          console.log(this.message);
-        }else{
-          console.log('Error al guardar')
-        }
-      },
-      error => {
-        console.log(<any>error);
+  onSubmit() {
+    if (this.hola) {
+      if (
+        this.person.FirstName != "" &&
+        this.person.SecondName != "" &&
+        this.person.Surname != "" &&
+        this.person.SecondSurname != "" &&
+        this.person.Date != "" &&
+        this.person.Religion != "" &&
+        this.person.Gender != "" &&
+        this.person.Email != "" &&
+        this.person.Departament != "" &&
+        this.person.Municipality != "" &&
+        this.person.Zone != "" &&
+        this.person.Colony != "" &&
+        this.person.Avenue != "" &&
+        this.person.Street != "" &&
+        this.person.Block != "" &&
+        this.person.HouseNumber != "" &&
+        this.person.Mobile != "" &&
+        this.person.Phone != "" &&
+        this.person.Other != ""
+      ) {
+        console.log(this.person);
+        this.rest.setPerson(this.person).subscribe(
+          response => {
+            if (response) {
+              this.message();
+              console.log(this.message);
+            } else {
+              console.log("Error al guardar");
+            }
+          },
+          error => {
+            console.log(<any>error);
+          }
+        );
+      } else {
+        alert("Debe llenar todos los campos necesarios para continuar");
       }
-    )
-    
+    } else {
+      console.log("no");
+    }
   }
 }
