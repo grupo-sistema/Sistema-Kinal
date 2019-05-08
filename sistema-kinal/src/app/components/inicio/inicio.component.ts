282c893c73941a30a58242133a2de7f22b155c4c
JavierCF07 <33670804+JavierCF07@users.noreply.github.com>
2019-05-07 22:01:47 -0600

    Cambios

--- sistema-kinal/src/app/components/inicio/inicio.component.ts
+++ sistema-kinal/src/app/components/inicio/inicio.component.ts
@@ -1,10 +1,10 @@
 import { Input, Component, OnInit } from "@angular/core";
 import { Person } from "src/app/models/person";
 import { RestService } from "../../services/rest.service";
-import Swal from 'sweetalert2';
+import Swal from "sweetalert2";
 import { checkAndUpdateBinding } from "@angular/core/src/view/util";
 import { Message } from "@angular/compiler/src/i18n/i18n_ast";
-import { CodigosService } from 'src/app/services/api/codigos.service';
+import { CodigosService } from "src/app/services/api/codigos.service";
 
 @Component({
   selector: "app-inicio",
@@ -33,65 +33,69 @@ export class InicioComponent implements OnInit {
   }
 
   // =========================================================================Funciones=============================================================
-  
+
   message() {
-    Swal.fire("Datos guardados correctamente", "You clicked the button!", "success")
+    if (
+      this.person.FirstName != "" || this.person.SecondName != "" || this.person.Surname != "" || this.person.SecondSurname != "" || this.person.Date != "" || this.person.Religion != "" || this.person.Gender != "" || this.person.Departament != "" || this.person.Municipality != "") {
+      Swal.fire("Datos guardados correctamente","You clicked the button!","success");
+    } else {
+      Swal.fire("Debe llenar todos los campos necesarios para continuar");
+    }
   }
 
-  guardarMobile(){
-    if(this.mobilesGuardados.length == 3){
-      console.log("Ya no puedes guardar mas mobiles")
-    }else{
-      if(this.mobile == "" || this.mobile == undefined){
+  guardarMobile() {
+    if (this.mobilesGuardados.length == 3) {
+      console.log("Ya no puedes guardar mas mobiles");
+    } else {
+      if (this.mobile == "" || this.mobile == undefined) {
         console.log("No has metido datos");
-      }else{
-        this.mobilesGuardados.push(this.mobile)
-        this.numerosGuardados.push(this.mobile)
-        console.log(this.numerosGuardados)
+      } else {
+        this.mobilesGuardados.push(this.mobile);
+        this.numerosGuardados.push(this.mobile);
+        console.log(this.numerosGuardados);
       }
     }
   }
 
-  guardarPhone(){
-    if(this.phonesGuardados.length == 3){
-      console.log("Ya no puedes guardar mas telefonos")
-    }else{
-      if(this.phone == "" || this.phone == undefined){
+  guardarPhone() {
+    if (this.phonesGuardados.length == 3) {
+      console.log("Ya no puedes guardar mas telefonos");
+    } else {
+      if (this.phone == "" || this.phone == undefined) {
         console.log("No has metido datos");
-      }else{
-        this.phonesGuardados.push(this.phone)
-        this.numerosGuardados.push(this.phone)
-        console.log(this.numerosGuardados)
+      } else {
+        this.phonesGuardados.push(this.phone);
+        this.numerosGuardados.push(this.phone);
+        console.log(this.numerosGuardados);
       }
     }
   }
 
-  guardarOther(){
-    if(this.othersGuardados.length == 3){
-      console.log("Ya no puedes guardar otros números")
-    }else{
-      if(this.other == "" || this.other == undefined){
+  guardarOther() {
+    if (this.othersGuardados.length == 3) {
+      console.log("Ya no puedes guardar otros números");
+    } else {
+      if (this.other == "" || this.other == undefined) {
         console.log("No has metido datos");
-      }else{
-        this.othersGuardados.push(this.other)
-        this.numerosGuardados.push(this.other)
-        console.log(this.other)
+      } else {
+        this.othersGuardados.push(this.other);
+        this.numerosGuardados.push(this.other);
+        console.log(this.other);
       }
     }
   }
 
-  guardarCorreos(){
-    if(this.correosGuardados.length == 3){
-      console.log("Ya no puedes guardar mas datos")
-    }
-    else{
-      if(this.email == "" || this.email == undefined){
+  guardarCorreos() {
+    if (this.correosGuardados.length == 3) {
+      console.log("Ya no puedes guardar mas datos");
+    } else {
+      if (this.email == "" || this.email == undefined) {
         console.log("No has metido datos");
-      }else{
-        this.correosGuardados.push(this.email)
-        console.log(this.correosGuardados)
+      } else {
+        this.correosGuardados.push(this.email);
+        console.log(this.correosGuardados);
       }
-    } 
+    }
   }
 
   ngOnInit() {
@@ -99,33 +103,31 @@ export class InicioComponent implements OnInit {
   }
 
   onSubmit() {
-    if(this.hola){
-      if (this.person.FirstName != "" && this.person.SecondName != "" && this.person.Surname != "" && this.person.SecondSurname != "" && this.person.Date != "" && this.person.Religion != "" && this.person.Gender != "" && this.person.Departament != "" &&this.person.Municipality != "") {
-        console.log(this.person);
-        this.person.Email = this.correosGuardados;
-        this.rest.setPerson(this.person).subscribe(
-          response => {
-            if (response) {
-              this.message();
-            } else {
-              console.log("Error al guardar");
-            }
-          },
-          error => {
-            console.log(<any>error);
+    if (this.hola) {
+      console.log(this.person);
+      this.rest.setPerson(this.person).subscribe(
+        response => {
+          if (response) {
+            this.message();
+          } else {
+            console.log("Error al guardar");
           }
-        );
-      } else {
-        Swal.fire("Debe llenar todos los campos necesarios para continuar");
-      }
-    }else{
+        },
+        error => {
+          console.log(<any>error);
+        }
+      );
+    } else {
     }
   }
 
   //Combobox de paises solamente de america (Se pueden agregar mas)
-  obtenerData(){
-  fetch('http://restcountries.eu/rest/v1/region/americas')
-  .then(response => response.json())
-  .then(json =>  {this.countries = json; console.log(this.countries)})
+  obtenerData() {
+    fetch("http://restcountries.eu/rest/v1/region/americas")
+      .then(response => response.json())
+      .then(json => {
+        this.countries = json;
+        console.log(this.countries);
+      });
   }
 }
